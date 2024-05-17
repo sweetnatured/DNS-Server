249926815cdb57b4f9d940321a60401769fddb73
salih <salih.beskardes@solactive.com>
2024-05-17 21:37:43 +0200

    created header

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket
 
+def create_message_header() -> bytes:
+    # headers are 12 bytes long
+    # packet ID is 16 bits long or 2 bytes, expected value is 1234
+    # Query/Response Indicator is 1 bit long, expected value is 1
+    res = [0 for _ in range(12)]
+    res[0] = (1234 >> 8) & 255
+    res[1] = 1234 & 255
+    res[2] = 128
+    return bytes(res)
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -14,7 +23,8 @@ def main():
         try:
             buf, source = udp_socket.recvfrom(512)
 
-            response = b""
+            response = create_message_header()
+            print(response)
 
             udp_socket.sendto(response, source)
         except Exception as e:
