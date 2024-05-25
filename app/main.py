e5db1ae073a273c041966f032656883864b3eb26
salih <salih.beskardes@solactive.com>
2024-05-25 20:47:50 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
+import argparse
 import socket
-from .header import Header, RCode
-from .message import DNSMessage
-from .question import DNSQuestion, QClass, QType
+from copy import deepcopy
+
 from .answer import Answer
+from .header import RCode
+from .message import DNSMessage
 
 
-def main():
+def main(resolver: str| None):
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
@@ -19,34 +21,30 @@ def main():
             data, source = udp_socket.recvfrom(512)
             request = DNSMessage.decode(data)
             print("request decoded")
-            response_header = Header(
-                id=request.header.id,
-                qr=1,
-                opcode=request.header.opcode,
-                aa=0,
-                tc=0,
-                rd=request.header.rd,
-                ra=0,
-                z=0,
-                rcode=RCode(4) if request.header.opcode else RCode(0),
-                qdcount=0,
-                ancount=0,
-                nscount=0,
-                arcount=0,
-            )
-            print("header created")
-            response = DNSMessage(header=response_header)
+
+            records = []
+            if resolver:
+                ip, port = resolver.split(":")
+                port = int(port)
+                rheader = deepcopy(request.header)
+                rheader.qdcount = 1
+                records = []
+                for question in request.questions:
+                    message = rheader.encode() + question.encode()
+                    resolver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    resolver_socket.sendto(message, (ip, port))
+                    data, _ = resolver_socket.recvfrom(512)
+                    resolver_response = DNSMessage.decode(data)
+                    records.extend(resolver_response.answers)
+            request.header.qr = 1
+            request.header.qdcount = request.header.ancount = 0
+            request.header.rcode = RCode(4) if request.header.opcode else RCode(0)
+            response = DNSMessage(header=request.header)
 
             for question in request.questions:
                 response.add_question(question)
-                response.add_answer(Answer(
-                    qname=question.qname,
-                    qtype=question.qtype,
-                    qclass=question.qclass,
-                    ttl=60,
-                    rdlength=4,
-                    rdata="8.8.8.8"
-                ))
+            for record in records:
+                response.add_answer(record)
 
             udp_socket.sendto(response.encode(), source)
 
@@ -56,4 +54,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--resolver", type=str, help="specify the DNS resolver address")
+    args = parser.parse_args()
+    main(args.resolver)
